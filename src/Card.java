abd35a58bcbfcdfcfd4d11602b92b2c0ba5e343b
andibley <andi_bley25@milton.edu>
2023-05-30 11:25:07 -0400

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,22 @@
+public class Card {
+  public String suit;
+  public int value;
+  public String name;
+  public boolean isDealt;
+
+
+    public Card(int pSuit,int pName){
+if(pSuit == 0){
+    suit = "dimond";
+    if(pSuit == 1){
+        suit = "club";
+        if(pSuit == 2){
+            suit = "hearts";
+            if(pSuit == 3){
+              
+            }
+        }
+    }
+}
+    }
+}
